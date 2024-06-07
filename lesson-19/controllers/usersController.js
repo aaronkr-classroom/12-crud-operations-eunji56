1bf21e2e8a8c593b4b57a727d3ad939ffc49d5f7
eunji56 <163230820+eunji56@users.noreply.github.com>
2024-06-07 04:17:02 +0000

    19끝

--- lesson-19/controllers/usersController.js
+++ lesson-19/controllers/usersController.js
@@ -37,6 +37,37 @@ module.exports = {
    * userController.js에 액션 생성 추가
    */
   // 폼의 렌더링을 위한 새로운 액션 추가
+  new: (req, res) => {
+    res.render("users/new");
+  },
+  create: (req, res, next) =>{
+    let userParams = {
+      name: {
+        first: req.body.first,
+        last: req.body.last, 
+      },
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      profileImg: req.body.profileImg,
+    };
+    User.create(userParams)
+    .then(user => {
+      console.log(`Created user: ${user.name}`);
+      res.locals.redirect = "/users";
+      res.locals.user = user;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error Created user: ${error.message}`);
+      next(error);
+    });
+  },
+  redirectView: (req, res, next) => {
+    let path = res.locals.redirect;
+    if (path) res.redirect(path);
+    else next();
+  },
   /**
    * @TODO: new, create, redirectView 액션을 객체 리터럴로 묶어 익스포트
    */
@@ -54,4 +85,19 @@ module.exports = {
   /**
    * @TODO: show, showView 액션을 객체 리터럴로 묶어 익스포트
    */
+  show: (req, res, next) => {
+    let userId = req.params.id; //URL의 매개변수 (/user/:id)
+    User.findById(userId)
+    .then(user => {
+      res.locals.user = user;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error fetching user: ${error.message}`);
+      next(error);
+    });
+  },
+  showView: (req, res) => {
+    res.render("users/show");
+  }
 };
